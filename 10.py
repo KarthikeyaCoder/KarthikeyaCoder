6f53f9b411cb299d161b505155d43c11bf6f6be1
Karthikeya <karthikeyarao615@gmail.com>
2026-01-22 20:20:38 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,14 @@
+num1=int(input("enter first no."))
+num2=int(input("enter second no."))
+oper=input("enter oper:")
+if oper=="+":
+    print(num1+num2)
+elif oper=="-":
+    print(num1-num2)
+elif oper=="*":
+    print(num1*num2)
+elif oper=="/":
+    print(num1/num2)
+else:
+    print("enter proper oper")
+
